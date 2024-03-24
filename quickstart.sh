616fbd02480b83de120e5a06088c7c36c50d1369
Viniciuscoding <vin.idem@gmail.com>
2024-03-24 14:34:26 -0400

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 
 # [START cloudbuild_quickstart_build_sh]
-echo "Hello, world! The time is $(date). Please work! Please work now!!!!"
+echo "Hello, world! The time is $(date). Please work! Please work now!!!! NOW!!!!!!"
 # [END cloudbuild_quickstart_build_sh]
